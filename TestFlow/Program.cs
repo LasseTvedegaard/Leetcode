98ef6a882cf01a88ae98cec372a960afe5f55079
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-04-20 15:45:04 +0200

    Add project files.

---
+++ TestFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace TestFlow {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] nums = { 2, 7, 3, 9, 8 };
+            int target = 10;
+
+            int[] result = TwoSum(nums, target);
+
+            if (result != null) {
+                Console.WriteLine("Indexes: [{0}, [{1}]", result[0], result[1]);
+            } else {
+                Console.WriteLine("No match");
+            }
+        }
+
+        static int[] TwoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.Length; i++) {
+                for (int j = i+1; j < nums.Length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
