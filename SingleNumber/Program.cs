c3cdb1b48c6b0c4fa632d9ff900f4760dae670df
LasseTN <103989770+LasseTN@users.noreply.github.com>
2024-04-24 21:35:47 +0200

    Single number

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Solution {
+    public int SingleNumber(int[] nums) {
+        int result = 0;
+        foreach (int num in nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
---
+++ TestFlow7/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TestFlow7 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] nums = {2, 2, 3, 3, 4};
+            Solution solution = new Solution();
+            int result = solution.SingleNumber(nums);
+            Console.WriteLine("The single number is: " + result);
+        }
+    }
+
+    public class Solution {
+        public int SingleNumber(int[] nums) {
+            int result = 0;
+            foreach (int num in nums) {
+                result ^= num;
+            }
+            return result;
+        }
+    }
+}
